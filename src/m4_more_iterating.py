8b31d719f8f95f590e48c6afda0c280828812faa
smithkg <smithkg@rose-hulman.edu>
2018-12-19 14:19:38 -0500

    done

--- src/m4_more_iterating.py
+++ src/m4_more_iterating.py
@@ -35,9 +35,9 @@ import math
 def main():
     """ Calls the   TEST   functions in this module. """
     # run_test_sum_sequence()
-    run_test_count_items_bigger_than()
+    # run_test_count_items_bigger_than()
     # run_test_count_positive_sines()
-    # run_test_sum_first_n()
+    run_test_sum_first_n()
 
 
 def run_test_sum_sequence():
@@ -371,7 +371,7 @@ def run_test_count_items_bigger_than():
     expected = 7
     actual = count_items_bigger_than(sequence, threshold)
     print()
-    print('Test 1 expected:', expected)
+    print('Test 9 expected:', expected)
     print('       actual:  ', actual)
 
     sequence = [0, 3, -7, 6, 15, -100, 4, 78]
@@ -379,7 +379,7 @@ def run_test_count_items_bigger_than():
     expected = 6
     actual = count_items_bigger_than(sequence, threshold)
     print()
-    print('Test 1 expected:', expected)
+    print('Test 10 expected:', expected)
     print('       actual:  ', actual)
 
 
@@ -431,7 +431,7 @@ def count_items_bigger_than(numbers, threshold):
 def run_test_count_positive_sines():
     """ Tests the   count_positive_sines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  count_positive_sines  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond what we supplied.
     #
@@ -528,6 +528,13 @@ def run_test_count_positive_sines():
 
     # TO DO 6 (continued):  Add your 1 ADDITIONAL test here:
 
+    # Test 11:
+    expected = 3
+    actual = count_positive_sines([3, 4, 5, 8, 9])
+    print()
+    print('Test 11 expected:', expected)
+    print('       actual:  ', actual)
+
 
 def count_positive_sines(numbers):
     """
@@ -553,15 +560,21 @@ def count_positive_sines(numbers):
       :type sequence: list or tuple (of numbers)
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
+
+    count = 0
+    for k in range(len(numbers)):
+        if math.sin(numbers[k]) > 0:
+            count = count + 1
+    return count
 
 
 def run_test_sum_first_n():
     """ Tests the   sum_first_n   function. """
     # -------------------------------------------------------------------------
-    # TODO: 8. Implement this TEST function.
+    # DONE: 8. Implement this TEST function.
     #   It TESTS the  sum_first_n  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests.
     #
@@ -632,6 +645,20 @@ def run_test_sum_first_n():
 
     # TO DO 8 (continued):  Add your 2 ADDITIONAL tests here:
 
+    # Test 8:
+    expected = 15
+    actual = sum_first_n([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual:  ', actual)
+
+    # Test 9:
+    expected = 55
+    actual = sum_first_n([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
+    print()
+    print('Test 9 expected:', expected)
+    print('       actual:  ', actual)
+
 
 def sum_first_n(numbers, n):
     """
@@ -655,7 +682,7 @@ def sum_first_n(numbers, n):
       :type n: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 9. Implement and test this function.
+    # DONE: 9. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # RESTRICTION:
@@ -664,6 +691,10 @@ def sum_first_n(numbers, n):
     #      -- The TESTING code above does use   built_ins.sum
     #         as an ORACLE in TESTING this function, however.
     # -------------------------------------------------------------------------
+    total = 0
+    for k in range(n):
+        total = total + numbers[k]
+    return total
 
 
 # -----------------------------------------------------------------------------
